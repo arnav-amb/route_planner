158e3c40b53c0e3b84c97fa4cc8c5862d3f33364
nolanding <deekshachandvani@gmail.com>
2018-03-19 23:31:15 +0530

    changes in algo.py

--- misc/algo.py
+++ misc/algo.py
@@ -16,42 +16,69 @@ import json
 # Google Places API
 # Google Geocoding API
 
-new_api = 'https://maps.googleapis.com/maps/api/geocode/json?'
+#new_api = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
 api_key = 'AIzaSyAD0tsB11_bi7ofAvU-M2S459wmPRkOlYY'
-place_request= 'address={}&key={}'.format('hawa mahal, jaipur', api_key)
+#place_request= 'address={}&key={}'.format('hawa mahal, jaipur', api_key)
 
-request = new_api + place_request
-# response = urllib.request.urlopen(request).read()
-r = requests.get(request)
-print(r)
-f= open('input.json','w+')
-f.write(r.text)
-f.close()
-#using search place api
+#request = new_api + place_request
+#r = requests.get(request)
+# print(r)
+# f= open('input.json','w+')
+# f.write(r.text)
+# f.close()
+# #using search place api
 
-endpoint = 'https://maps.googleapis.com/maps/api/place/details/json?'
+#endpoint = 'https://maps.googleapis.com/maps/api/place/details/json?'
 with open('input.json','r') as myfile:
 	content=json.load(myfile)
-print(content)			
+newcontent=content['results'][0]
+more=newcontent['geometry']
+late=more['location']
+latitude = late['lat']
+longitute = late['lng']
+place_id= newcontent["place_id"]
+
+# for x in content: 
+# 	for y in content[1]:
+# 		print(y)
+# 		print("\n")			
 #getObjects(content,"gemeotry","location")
 #id=content[53][23:50]
 #res_dic = json.loads(content.results)
 #id = res_dict['place_id']
 
-place_ask = 'key={}&place_id={}'.format(api_key,id)
+#place_ask = 'key={}&place_id={}'.format(api_key,place_id)
 
-request_2 = endpoint + place_ask
+#request_2 = endpoint + place_ask
 
-r_2=requests.get(request_2)
-print(r_2)
-f=open('user.json','w+')
-f.write(r_2.text)
+#r_2=requests.get(request_2)
+#print(r_2)
+#f=open('user.json','w+')
+#f.write(r_2.text)
+#f.close()
+
+#near_by='https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+
+#nearby_ask = 'key={}&latitude={}&longitude={}&radius={}&type={}'.format(api_key,latitude,longitute,'10000','restaurant')
+
+# request_3 = near_by + nearby_ask
+# r_3=requests.get(request_3)
+# f=open('places.json','w+')
+# f.write(r_3.text)
+# f.close()
+
+# print(r_3)
+
+path='https://maps.googleapis.com/maps/api/directions/json?'
+
+app_key='AIzaSyAqwxsqZQbDezGD_V-egxM4kxzm-0bpQ_8'
+path_var='origin={}&destination={}&key={}'.format('hawa mahal','jal mahal',app_key)
+
+request_3 = path + path_var
+r_3=requests.get(request_3)
+f=open('places.json','w+')
+f.write(r_3.text)
 f.close()
-
-
-
-
-
 
 
