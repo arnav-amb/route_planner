e477dbe19418eeae1b1ef5072abe494fdc601e27
nolanding <deekshachandvani@gmail.com>
2018-03-20 09:49:54 +0530

    updated algo

--- misc/algo.py
+++ misc/algo.py
@@ -39,15 +39,7 @@ latitude = late['lat']
 longitute = late['lng']
 place_id= newcontent["place_id"]
 
-# for x in content:
-# 	for y in content[1]:
-# 		print(y)
-# 		print("\n")
-#getObjects(content,"gemeotry","location")
-#id=content[53][23:50]
-#res_dic = json.loads(content.results)
-#id = res_dict['place_id']
-#
+# 
 # place_ask = 'key={}&place_id={}'.format(api_key,place_id)
 #
 # request_2 = endpoint + place_ask
@@ -58,36 +50,50 @@ place_id= newcontent["place_id"]
 # f.write(r_2.text)
 # f.close()
 
-near_by='https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+#api for getting places to visit nearby
+
+# near_by='https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
 
 places=['restaurant','market','monument','temples','mosque']
-f=open('near_places.json','w+')
+# i=0
+# while i < len(places):
+# 	#print(places[i])
+# 	nearby_ask = 'key={}&location={},{}&radius={}&keyword={}'.format(api_key,latitude,longitute,'10000',places[i])
+# 	#print(nearby_ask)
+# 	request_4 = near_by + nearby_ask
+# 	#print(request_4)
+# 	r_4=requests.get(request_4)
+# 	f=open('near_places'+str(i)+'.json','w+')
+# 	f.write(r_4.text)
+# 	f.close()
+# 	i+=1
+
+
+# # print(r_3)
+
+#api for getting directions between two places
+
+# path='https://maps.googleapis.com/maps/api/directions/json?'
+
+# app_key='AIzaSyAqwxsqZQbDezGD_V-egxM4kxzm-0bpQ_8'
+# path_var='origin={}&destination={}&key={}'.format('hawa mahal','jal mahal',api_key)
+
+# request_3 = path + path_var
+# r_3=requests.get(request_3)
+# f=open('places.json','w+')
+# f.write(r_3.text)
+# f.close()
+
+#api ends
 i=0
 while i < len(places):
-	print(places[i])
-	nearby_ask = 'key={}&location={},{}&radius={}&keyword={}'.format(api_key,latitude,longitute,'10000',places[i])
-	print(nearby_ask)
-	request_4 = near_by + nearby_ask
-	print(request_4)
-	r_4=requests.get(request_4)
-	f.write(r_4.text)
-	i+=1
-f.close()
-
-# print(r_3)
+	with open('near_places'+str(i)+'.json', 'r') as data_file:
+		data=json.load(data_file)
+	for j in data:
+		results = j{'results'}
+		place_name = j{'name'}
+		print(place_name)
 
 
-path='https://maps.googleapis.com/maps/api/directions/json?'
-
-app_key='AIzaSyAqwxsqZQbDezGD_V-egxM4kxzm-0bpQ_8'
-path_var='origin={}&destination={}&key={}'.format('hawa mahal','jal mahal',api_key)
-
-request_3 = path + path_var
-r_3=requests.get(request_3)
-f=open('places.json','w+')
-f.write(r_3.text)
-f.close()
-<<<<<<< HEAD
-
-=======
->>>>>>> 23a64c238fe0656f6f075382990fb1d302b8ef30
+	i+=1	
+#
